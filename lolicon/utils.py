34b6198ac269d9fec353baae9bcc22e0977edbb2
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2020-12-29 20:33:04 +0100

    Move resource loading to utils file

--- lolicon/utils.py
+++ lolicon/utils.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
-"""
-TODO: Add utils here.
-"""
+from __future__ import annotations
+
+import json
+from importlib.resources import path as resource_path
+from typing import List
+
+
+def load_resource(resource: str, package: str) -> List[dict]:
+     with resource_path(resource, package) as resource_handler:
+         with open(resource_handler, mode='r', encoding='utf-8') as file_handler:
+             return json.load(file_handler)
