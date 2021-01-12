9b1fc5a7688a46d6e06def3c9760164a8d6a1ab4
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-01-12 17:34:09 +0100

    Cover more edge cases in str_to_bool and reformat code

--- lolicon/utils.py
+++ lolicon/utils.py
@@ -7,7 +7,6 @@ import json
 import sqlite3
 from contextlib import closing
 from importlib.resources import path as resource_path
-from sqlite3.dbapi2 import Cursor, connect
 from typing import Iterable, List
 
 import pint
@@ -16,15 +15,13 @@ from colorama import Fore, Style
 UNIT = pint.UnitRegistry()
 
 
-def str_to_bool(string) -> bool:
-    return True if string == 'True' else False
-
 def load_resource(resource: str, package: str) -> List[dict]:
-     with resource_path(resource, package) as resource_handler:
-         with open(resource_handler, mode='r', encoding='utf-8') as file_handler:
-             return json.load(file_handler)
+    with resource_path(resource, package) as resource_handler:
+        with open(resource_handler, mode='r', encoding='utf-8') as file_handler:
+            return json.load(file_handler)
 
-def query_db(db: str, sql: str, parameters: Iterable) -> List:    
+
+def query_db(db: str, sql: str, parameters: Iterable) -> List:
     with resource_path('lolicon.data', db) as resource_handler:
         with closing(sqlite3.connect(resource_handler)) as connection:
             with closing(connection.cursor()) as cursor:
@@ -32,14 +29,24 @@ def query_db(db: str, sql: str, parameters: Iterable) -> List:
 
 
 def raise_on_none(variable: str):
-        def decorator(func):
-            @functools.wraps(func)
-            def wrapper(*args, **kwargs):
-                try:
-                    if func(*args, **kwargs) is None:
-                        raise ValueError(f"{Fore.RED}{variable} is None{Style.RESET_ALL}")
-                    return func(*args, **kwargs)
-                except TypeError:
-                    raise ValueError(f"{Fore.RED}{variable} is None{Style.RESET_ALL}")
-            return wrapper
-        return decorator
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                if func(*args, **kwargs) is None:
+                    raise ValueError(
+                        f"{Fore.RED}{variable} is None{Style.RESET_ALL}")
+                return func(*args, **kwargs)
+            except TypeError:
+                raise ValueError(
+                    f"{Fore.RED}{variable} is None{Style.RESET_ALL}")
+        return wrapper
+    return decorator
+
+
+@raise_on_none('string')
+def str_to_bool(string: str) -> bool:
+    """
+    Convert string to boolean if string is not `None`, else raise `ValueError`.
+    """
+    return (string.capitalize() == 'True' or string.capitalize() == 'Yes') if string is not None else None
