99d4bd7c52c3bf9a19d86c97cee2654a2368664d
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-02-04 23:29:35 +0100

    Rename/remove old files

--- lolicon/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from .chemistry import *
-from .lolicon import *
-from .physics import *
-
-__version__ = "0.0.4a"
-package_name = "lolicon"
--- lolicon/lolicon.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-"""
-TODO: Add GUI here.
-"""
-
