91c81e67fa2a8815b1e8db1eee7046f7b583c73f
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-02-04 23:29:08 +0100

    Migrate setup.cfg to setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,68 @@
 #!/usr/bin/env python
 import re
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
-with open("lolicon/__init__.py", encoding='utf8') as file_handler:
+with open("src/lolicon/__init__.py", encoding='utf-8') as file_handler:
     lines = file_handler.read()
     version = re.search(r'__version__ = "(.*?)"', lines).group(1)
     package_name = re.search(r'package_name = "(.*?)"', lines).group(1)
+    python_major = int(re.search(r'python_major = "(.*?)"', lines).group(1))
+    python_minor = int(re.search(r'python_minor = "(.*?)"', lines).group(1))
 
-with open("requirements.txt", encoding='utf-8') as file_handler:
-    packages = file_handler.read().splitlines()
+print("reading dependency file")
 
-# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+with open("requirements/release.txt", mode='r', encoding='utf-8') as requirements:
+    packages = requirements.read().splitlines()
+
+with open("requirements/dev.txt", mode='r', encoding='utf-8') as requirements:
+    dev_packages = requirements.read().splitlines()
+
+print("reading readme file")
+
+with open("README.md", mode='r', encoding='utf-8') as readme:
+    long_description = readme.read()
+
+print("running %s's setup routine" % package_name)
+
 setup(
+    author='hentai-chan',
+    author_email="dev.hentai-chan@outlook.com",
     name=package_name,
     version=version,
+    description="A STEM project that provides useful utility methods for engineering sciences and educational purposes.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://hentaichan.pythonanywhere.com/projects/lolicon",
+    project_urls={
+        'Documentation': "https://hentaichan.pythonanywhere.com/projects/lolicon",
+        'Source Code': "https://github.com/hentai-chan/lolicon",
+        'Bug Reports': "https://github.com/hentai-chan/lolicon/issues",
+        'Changelog': "https://github.com/hentai-chan/lolicon/blob/master/CHANGELOG.md"
+    },
+    python_requires=">=%d.%d" % (python_major, python_minor),
     install_requires=packages,
-    include_package_data=True
+    extra_requires={
+        'dev': dev_packages[1:],
+        'test': ['pytest']
+    },
+    include_package_data=True,
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+        'Topic :: Education',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities'
+    ],
+    keywords="math physics chemistry engineering computer science"
 )
+
+wheel_name = package_name.replace('-', '_') if '-' in package_name else package_name
+print("\033[92mSetup is complete. Run 'python -m pip install dist/%s-%s-py%d-none-any.whl' to install this wheel.\033[0m" % (wheel_name, version, python_major))
