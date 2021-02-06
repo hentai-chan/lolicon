ef51579c9f506efc633451da7d4d1b10a9cebca0
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-02-06 20:15:45 +0100

    Preemptively add python version check

--- src/lolicon/__init__.py
+++ src/lolicon/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import sys
+
 from .chemistry import *
 from .lolicon import *
 from .physics import *
@@ -8,3 +10,8 @@ __version__ = "0.0.5a"
 package_name = "lolicon"
 python_major = "3"
 python_minor = "7"
+
+try:
+    assert sys.version_info >= (int(python_major), int(python_minor))
+except AssertionError:
+    raise RuntimeError(f"\033[91m{package_name.capitalize()} requires Python {python_major}.{python_minor}+ (You have Python {sys.version})\033[0m")
