7966e0881c5444338d3da9c887fa1f5ef8eb4eaf
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-02-06 21:15:50 +0100

    Add log handler for this module and implement a config path util method.

--- src/lolicon/utils.py
+++ src/lolicon/utils.py
@@ -2,13 +2,17 @@
 
 from __future__ import annotations
 
-import warnings
 import functools
 import json
+import logging
+import platform
 import sqlite3
+import warnings
 from contextlib import closing
 from importlib.resources import path as resource_path
-from typing import Iterable, List
+from os.path import expandvars
+from pathlib import Path
+from typing import List
 
 import pint
 from colorama import Fore, Style
@@ -16,18 +20,32 @@ from colorama import Fore, Style
 UREG = pint.UnitRegistry()
 
 
+def _hidden_module_path(target_dir: str) -> Path:
+    """
+    Return the base config path for this module.
+    """
+    directory = Path(expandvars('%LOCALAPPDATA%')) if platform.system() == 'Windows' else Path('/etc')
+    directory = directory.joinpath(target_dir)
+    directory.mkdir(parents=True, exist_ok=True)
+    return directory
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s::%(levelname)s::%(lineno)d::%(name)s::%(message)s', datefmt='%d-%b-%y %H:%M:%S')
+file_handler = logging.FileHandler(_hidden_module_path(target_dir='.lolicon').joinpath('lolicon.log'))
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
 def load_resource(resource: str, package: str) -> List[dict]:
     with resource_path(resource, package) as resource_handler:
         with open(resource_handler, mode='r', encoding='utf-8') as file_handler:
             return json.load(file_handler)
-
 
 def query_db(db: str, sql: str, *args, local_: bool=False) -> List:
     with resource_path('src.lolicon.data' if local_ else 'lolicon.data', db) as resource_handler:
         with closing(sqlite3.connect(resource_handler)) as connection:
             with closing(connection.cursor()) as cursor:
                 return cursor.execute(sql, *args).fetchall()
-
 
 def raise_on_none(variable: str):
     def decorator(func):
