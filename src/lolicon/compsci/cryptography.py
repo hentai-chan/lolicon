f179cb7a32b8d12b452c9940846247728d6e9c91
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-02-09 19:35:55 +0100

    Extend placeholder function; test logging and warning mechanism

--- src/lolicon/compsci/cryptography.py
+++ src/lolicon/compsci/cryptography.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
-def hello_world() -> str:
+import string
+
+from .. import utils
+from ..utils import logger
+
+__warning_msg = "You're using an cryptographically insecure method."
+
+def hello_world(surpress_warning: bool=False) -> str:
+    utils.raise_warning(__warning_msg, surpress_warning)
+    logger.info('test logger!')
     return 'Hello, World!'
