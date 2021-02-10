de201d0001ca1f62615a9a437472caac58321826
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-02-10 22:21:56 +0100

    Remove test_hello_world and add test cases for more encryption and decryption method

--- tests/test_compsci.py
+++ tests/test_compsci.py
@@ -6,11 +6,11 @@ from src.lolicon.compsci import cryptography
 class ComputerScience(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        ...
+        pass
 
     @classmethod
     def tearDownClass(cls) -> None:
-        ...
+        pass
 
     def test_bin2dec(self):
         self.assertEqual(compsci.bin2dec('10110'), 22)
@@ -25,11 +25,27 @@ class ComputerScience(unittest.TestCase):
 class TestCryptography(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        ...
+        pass
 
     @classmethod
     def tearDownClass(cls):
-        ...
+        pass
 
-    def test_hello_world(self):
-        self.assertEqual(cryptography.hello_world(surpress_warning=True), 'Hello, World!')
+    def test_encrypt_morse(self):
+        # case-insensitive
+        source = 'Hello World'
+        cypher = '.... . .-.. .-.. --- .-- --- .-. .-.. -..'
+        self.assertEqual(cryptography.encrypt_morse(source), cypher)
+        # numeric support
+        source = 'The attack takes place at 2 AM'
+        cypher = '- .... . .- - - .- -.-. -.- - .- -.- . ... .--. .-.. .- -.-. . .- - ..--- .- --'
+        self.assertEqual(cryptography.encrypt_morse(source), cypher)
+
+    def test_decrypt_morse(self):
+        # result always in uppercase without stripped off all whitespaces
+        source = 'HELLOWORLD'
+        cypher = '.... . .-.. .-.. --- .-- --- .-. .-.. -..'
+        self.assertEqual(cryptography.decrypt_morse(cypher), source)
+        source = 'THEATTACKTAKESPLACEAT2AM'
+        cypher = '- .... . .- - - .- -.-. -.- - .- -.- . ... .--. .-.. .- -.-. . .- - ..--- .- --'
+        self.assertEqual(cryptography.decrypt_morse(cypher), source)
