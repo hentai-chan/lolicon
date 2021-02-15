9c9653cee4717f1ac65e378bcedeb6aa4b4c5a74
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-02-15 21:06:47 +0100

    Add test_mod_inverse to test suite

--- tests/test_mathematics.py
+++ tests/test_mathematics.py
@@ -4,7 +4,7 @@ import unittest
 
 from src.lolicon.mathematics import mathematics
 
-class ComputerMathematics(unittest.TestCase):
+class Mathematics(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         pass
@@ -16,3 +16,9 @@ class ComputerMathematics(unittest.TestCase):
     def test_gcd(self):
         self.assertEqual(mathematics.gcd(60, 48), 12)
         self.assertEqual(mathematics.gcd(32, 24), 8)
+        self.assertEqual(mathematics.gcd(12121, 37847374), 23)
+
+    def test_mod_inverse(self):
+        self.assertEqual(mathematics.mod_inverse(5, 7), 3)
+        self.assertEqual(mathematics.mod_inverse(763, 23), 6)
+        self.assertEqual(mathematics.mod_inverse(9438274, 40928773), 40286876)
