c27bda7ae0327308a1e02ffdd6f8cefcaf92762c
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-02-17 23:19:02 +0100

    Add zero padding arg to dec2bin and ensure that bin2dec returns integer values only

--- src/lolicon/compsci/compsci.py
+++ src/lolicon/compsci/compsci.py
@@ -4,16 +4,17 @@ from __future__ import annotations
 
 import math
 
-def dec2bin(dec: int) -> str:
+def dec2bin(dec: int, pad_zero: int=8) -> str:
     """
     Suppose that `dec` is an unsigned integer. Convert this number into its
-    binary representation.
+    binary representation. Adjust `pad_zero` to fill the binary number string
+    with zeros (set to one byte by default).
     """
     bin_ = []
     while dec != 0:
         dec, r = divmod(dec, 2)
         bin_.append(str(r))
-    return ''.join(reversed(bin_))
+    return ''.join(reversed(bin_)).rjust(pad_zero, '0')
 
 
 def bin2dec(bin_: str) -> int:
@@ -21,4 +22,4 @@ def bin2dec(bin_: str) -> int:
     Suppose that `bin` is a valid binary number. Convert this number into its
     decimal representation.
     """
-    return sum(map(lambda i: int(bin_[::-1][i]) * math.pow(2, i), range(len(bin_))))
+    return int(sum(map(lambda i: int(bin_[::-1][i]) * math.pow(2, i), range(len(bin_)))))
