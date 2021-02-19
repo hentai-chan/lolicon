e99581971c0b43c60c4bcb8ff25d040a007d5cba
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-02-19 22:43:19 +0100

    Rename keyword argument

--- src/lolicon/compsci/compsci.py
+++ src/lolicon/compsci/compsci.py
@@ -4,17 +4,17 @@ from __future__ import annotations
 
 import math
 
-def dec2bin(dec: int, pad_zero: int=8) -> str:
+def dec2bin(dec: int, padding: int=8) -> str:
     """
     Suppose that `dec` is an unsigned integer. Convert this number into its
-    binary representation. Adjust `pad_zero` to fill the binary number string
+    binary representation. Adjust `padding` to fill the binary number string
     with zeros (set to one byte by default).
     """
     bin_ = []
     while dec != 0:
         dec, r = divmod(dec, 2)
         bin_.append(str(r))
-    return ''.join(reversed(bin_)).rjust(pad_zero, '0')
+    return ''.join(reversed(bin_)).rjust(padding, '0')
 
 
 def bin2dec(bin_: str) -> int:
