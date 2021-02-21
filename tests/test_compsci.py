7daf3d7b5feac2ebcc0f755ef707497935ae2ea8
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-02-21 18:53:22 +0100

    Fix class name typo and shorten cryptography import to crypto

--- tests/test_compsci.py
+++ tests/test_compsci.py
@@ -5,10 +5,10 @@ import unittest
 
 import pytest
 import src.lolicon.compsci as compsci
-from src.lolicon.compsci import cryptography
+from src.lolicon.compsci import cryptography as crypto
 
 
-class ComputerScience(unittest.TestCase):
+class TestComputerScience(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         pass
@@ -41,58 +41,58 @@ class TestCryptography(unittest.TestCase):
         from_morse = lambda msg: msg.upper().replace(' ', '')
         # case-insensitive
         msg = 'Measure what is measurable and make measurable what is not so'
-        cypher = cryptography.encrypt_morse_code(msg)
-        self.assertEqual(cryptography.decrypt_morse_code(cypher), from_morse(msg))
+        cypher = crypto.encrypt_morse_code(msg)
+        self.assertEqual(crypto.decrypt_morse_code(cypher), from_morse(msg))
         # numeric support
         msg = 'I was x years old in the year x2'
-        cypher = cryptography.encrypt_morse_code(msg)
-        self.assertEqual(cryptography.decrypt_morse_code(cypher), from_morse(msg))        
+        cypher = crypto.encrypt_morse_code(msg)
+        self.assertEqual(crypto.decrypt_morse_code(cypher), from_morse(msg))        
 
     @pytest.mark.filterwarnings('ignore::UserWarning')
     def test_encrypt_binary(self):
         msg = "It's not that I'm so smart, it's just that I stay with problems longer."
-        cypher = cryptography.encrypt_binary(msg)
-        self.assertEqual(cryptography.decrypt_binary(cypher), msg)
+        cypher = crypto.encrypt_binary(msg)
+        self.assertEqual(crypto.decrypt_binary(cypher), msg)
 
     @pytest.mark.filterwarnings('ignore::UserWarning')
     def test_caesar_cypher(self):
         # default seed
         msg = 'mathematics is the handwriting on the human consciousness of the very spirit of life itself.'
-        cypher = cryptography.encrypt_caesar_cypher(msg, shift=2)
-        self.assertEqual(cryptography.decrypt_caesar_cypher(cypher, shift=2), msg)
+        cypher = crypto.encrypt_caesar_cypher(msg, shift=2)
+        self.assertEqual(crypto.decrypt_caesar_cypher(cypher, shift=2), msg)
         # uppercase only
         msg = 'THERE ARE NO CREEDS IN MATHEMATICS'
-        cypher = cryptography.encrypt_caesar_cypher(msg, shift=8,seed=string.ascii_uppercase)
-        self.assertEqual(cryptography.decrypt_caesar_cypher(cypher, shift=8, seed=string.ascii_uppercase), msg)
+        cypher = crypto.encrypt_caesar_cypher(msg, shift=8,seed=string.ascii_uppercase)
+        self.assertEqual(crypto.decrypt_caesar_cypher(cypher, shift=8, seed=string.ascii_uppercase), msg)
         # using special characters and numbers
         seed = ''.join((string.ascii_letters, string.digits, string.punctuation))
         msg = 'Mathematics is not a contemplative but a creative subject.'
-        cypher = cryptography.encrypt_caesar_cypher(msg, shift=13, seed=seed)
-        self.assertEqual(cryptography.decrypt_caesar_cypher(cypher, shift=13, seed=seed), msg)        
+        cypher = crypto.encrypt_caesar_cypher(msg, shift=13, seed=seed)
+        self.assertEqual(crypto.decrypt_caesar_cypher(cypher, shift=13, seed=seed), msg)        
 
     @pytest.mark.filterwarnings('ignore::UserWarning')
     def test_transposition_cypher(self):
         msg = 'The highest form of pure thought is in mathematics.'
-        cypher = cryptography.encrypt_transposition_cypher(msg, key=8)
-        self.assertEqual(cryptography.decrypt_transposition_cypher(cypher, key=8), msg)
+        cypher = crypto.encrypt_transposition_cypher(msg, key=8)
+        self.assertEqual(crypto.decrypt_transposition_cypher(cypher, key=8), msg)
 
     @pytest.mark.filterwarnings('ignore::UserWarning')
     def test_affine_cypher(self):
-        key = cryptography.generate_affine_key()
+        key = crypto.generate_affine_key()
         msg = 'The important thing to remember about mathematics is not to be frightened.'
-        cypher = cryptography.encrypt_affine_cypher(msg, key)
-        self.assertEqual(cryptography.decrypt_affine_cypher(cypher, key), msg)
+        cypher = crypto.encrypt_affine_cypher(msg, key)
+        self.assertEqual(crypto.decrypt_affine_cypher(cypher, key), msg)
 
     @pytest.mark.filterwarnings('ignore::UserWarning')
     def test_vigenere_cypher(self):
         # default settings
         key = 'jacqueline'
         msg = 'mathematics is the music of reason'
-        cypher = cryptography.encrypt_vigenere_cypher(msg, key)
-        self.assertEqual(cryptography.decrypt_vigenere_cypher(cypher, key), msg)
+        cypher = crypto.encrypt_vigenere_cypher(msg, key)
+        self.assertEqual(crypto.decrypt_vigenere_cypher(cypher, key), msg)
         # shuffled key with extended seed
         key = string.printable
         sorted(key)
         msg = "Go down deep enough into anything and you will find mathematics."
-        cypher = cryptography.encrypt_vigenere_cypher(msg, key, string.printable)
-        self.assertEqual(cryptography.decrypt_vigenere_cypher(cypher, key, string.printable), msg)
+        cypher = crypto.encrypt_vigenere_cypher(msg, key, string.printable)
+        self.assertEqual(crypto.decrypt_vigenere_cypher(cypher, key, string.printable), msg)
