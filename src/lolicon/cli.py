8c1da60fbb2c67abdcb3f442f008e7143375b194
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-03-01 01:16:21 +0100

    Increase color contrast in help messages

--- src/lolicon/cli.py
+++ src/lolicon/cli.py
@@ -14,15 +14,15 @@ from .__init__ import __version__, package_name
 CONTEXT_SETTINGS = dict(max_content_width=120)
 
 @click.group(invoke_without_command=True, context_settings=CONTEXT_SETTINGS, help=style("A modern CLI template for python scripts.", fg='bright_magenta'))
-@click.version_option(version=__version__, prog_name=package_name, help=style("Show the version and exit.", fg='yellow'))
+@click.version_option(version=__version__, prog_name=package_name, help=style("Show the version and exit.", fg='bright_yellow'))
 @click.pass_context
 def cli(ctx):
     ctx.ensure_object(dict)
 
 @cli.command(help=style("Perform log file operations.", fg='bright_green'), context_settings=CONTEXT_SETTINGS)
-@click.option('--read', is_flag=True, default=False, help=style("Read the log file.", fg='yellow'))
-@click.option('--reset', is_flag=True, default=False, help=style("Reset all log file entries", fg='yellow'))
-@click.option('--path', is_flag=True, default=False, help=style("Get the log file path.", fg='yellow'))
+@click.option('--read', is_flag=True, default=False, help=style("Read the log file.", fg='bright_yellow'))
+@click.option('--reset', is_flag=True, default=False, help=style("Reset all log file entries", fg='bright_yellow'))
+@click.option('--path', is_flag=True, default=False, help=style("Get the log file path.", fg='bright_yellow'))
 def log(read, reset, path):
     if read:
         utils.read_log()
